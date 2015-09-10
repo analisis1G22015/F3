c6538ef56d3e5a30b67cb3ffa8ef07957fef97e3
analisis1G22015 <alan220193@hotmail.com>
2015-09-09 21:15:31 -0600

    mock para simular servicio de banco

---
+++ compra/banco_test.php
@@ -0,0 +1,8 @@
+<?php
+class banco{	
+	function banco(){		
+	}	
+	function comprobar_saldo_tarjet($numero_tarjeta,$fecha_vencimiento,$codigo_seguridad,$nombre_titular,$monto_necesario){
+		//ir a la base de datos a buscar el #de tarjeta y retorna si cubre con el monto necesario
+	}
+}
